5e3e1bdf7cbeb8ad909db73c9314281bcc4e35fa
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2023-07-05 16:01:44 +0200

    #1 finished

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const body = document.querySelector("body");
+const startButton = document.querySelector("[data-start]");
+const stopButton = document.querySelector("[data-stop]");
 
+
+startButton.addEventListener("click", onClickStart);
+
+function onClickStart() {
+    timerId = setInterval(()=>{
+        const randomColor = getRandomHexColor();
+        body.style.backgroundColor = randomColor;
+    }, 1000);
+    startButton.disabled = true;
+}
+stopButton.addEventListener("click", onClickStop);
+function onClickStop() {
+    clearInterval(timerId);
+    startButton.disabled = false;
+}
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
